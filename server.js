3c2a44eec3a568cb632d12c4a06eaba74dca9258
shyss18 <proffsneip@gmail.com>
2021-12-13 17:10:38 +0300

    add express

---
+++ server.js
@@ -0,0 +1,7 @@
+const http = require("http");
+const app = require("./app");
+
+const port = process.env.port || process.env.PORT || 5000;
+
+const server = http.createServer(app);
+server.listen(port, () => console.log("server is up!"));
