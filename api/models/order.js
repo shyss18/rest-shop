2c6d2f66aeea6f4298c227cf20d3aa68d5542419
shyss18 <proffsneip@gmail.com>
2021-12-14 10:28:36 +0300

    add mongoose model validation

--- api/models/order.js
+++ api/models/order.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const orderSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     productId: mongoose.Types.ObjectId,
-    time: Date,
-    quantity: Number
+    time: { type: Date, required: true },
+    quantity: { type: Number, required: true }
 });
 
 module.exports = mongoose.model("Order", orderSchema);
